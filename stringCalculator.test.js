3496bfb5866823e569a3a6fd7c8d081aca5d0020
utk01 <88829084+utk01@users.noreply.github.com>
2022-04-11 02:09:15 +0530

    Second Test

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,7 @@ describe("stringCalculator",()=>{
     it("return 0 for empty string",()=>{
         expect(stringCalculator("")).toBe(0);
     });
+    it("if single number in string return number",()=>{
+        expect(stringCalculator("3")).toBe(3);
+    });
 });
