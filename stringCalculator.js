c8a924e12d05662d234435166402b2b18a32bd64
utk01 <88829084+utk01@users.noreply.github.com>
2022-04-11 02:45:59 +0530

    Fifth Test Passed

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,6 @@
 
 function stringCalculator(exp){
-    var numArray= exp.split(',').map(Number);
+    var numArray= exp.split(/,|\n/).map(Number);
     return (parseInt(numArray.reduce((a,b)=>a+b)));
 }
 
