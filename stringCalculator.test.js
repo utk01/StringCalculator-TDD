2427f3c985738dfb9e7758446f8a2da35059a560
utk01 <88829084+utk01@users.noreply.github.com>
2022-04-11 02:23:19 +0530

    Third Test

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -7,4 +7,8 @@ describe("stringCalculator",()=>{
     it("if single number in string return number",()=>{
         expect(stringCalculator("3")).toBe(3);
     });
+    it("if two comma seprated numbers in string return their sum",()=>{
+        expect(stringCalculator("1,3")).toBe(4);
+    });
+
 });
